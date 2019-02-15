cf82b5f877b5fdc494b99a77623d7557e1382923
qiaoyue <qiaoyue@baidu.com>
2019-02-15 21:07:48 +0800

    update

--- src/component/renderProps.tsx
+++ src/component/renderProps.tsx
@@ -3,6 +3,7 @@ import * as PropTypes from 'prop-types'
 
 /**]
  * @file 负责控制逻辑，直接渲染传入子组件
+ *       解决 hoc 面临的问题：无用标签，嵌套加深，结构不可见
  */
 
 export default class RenderProps extends React.Component<any, any> {
