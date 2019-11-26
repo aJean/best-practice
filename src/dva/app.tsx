f00a79b15ddac34481b521751096a05bd9d5a4ce
qiaoyue3 <qiaoyue3@jd.com>
2019-11-26 15:29:37 +0800

    update

--- src/dva/app.tsx
+++ src/dva/app.tsx
@@ -9,52 +9,49 @@ import ListPage from './list';
 const app = new Polymer();
 
 app.add({
-    ns: 'count',
-    state: {
-      record : 0,
-      current: 0
-    },
-    path: '/count',
-    name: '计数器',
-    component: HomePage,
-    reducers: {
-        add(state) {
-            const newCurrent = state.current + 1;
-            return { ...state,
-                record: newCurrent > state.record ? newCurrent : state.record,
-                current: newCurrent
-            };
-        }
+  ns: 'count',
+  state: {
+    record: 0,
+    current: 0
+  },
+  path: '/count',
+  name: '计数器',
+  component: HomePage,
+  reducers: {
+    add(state) {
+      const newCurrent = state.current + 1;
+      return { ...state, record: newCurrent > state.record ? newCurrent : state.record, current: newCurrent };
     }
+  }
 });
 
 app.add({
-    ns: 'list',
-    state: ['wwwwwwww', 'aaaaaaaaa', 'aaaaaaaaaaa'],
-    path: '/list',
-    name: '列表测试 saga',
-    component: ListPage,
-    effects: {
-        *add(action, { call, put }) {
-            yield call(delay, 1000);
-            yield put({ type: 'list/success', data: Date.now() });
-        }
-    },
-    reducers: {
-        success: function(state, action) {
-            return [...state, 'new' + action.data];
-        }
+  ns: 'list',
+  state: ['wwwwwwww', 'aaaaaaaaa', 'aaaaaaaaaaa'],
+  path: '/list',
+  name: '列表测试 saga',
+  component: ListPage,
+  effects: {
+    *add(action, { call, put }) {
+      yield call(delay, 1000);
+      yield put({ type: 'list/success', data: Date.now() });
     }
+  },
+  reducers: {
+    success: function(state, action) {
+      return [...state, 'new' + action.data];
+    }
+  }
 });
 
-function delay(timeout){
-    return new Promise(resolve => {
-        setTimeout(resolve, timeout);
-    });
+function delay(timeout) {
+  return new Promise(resolve => {
+    setTimeout(resolve, timeout);
+  });
 }
 
 export default {
-    init(el) {
-        app.start(el);
-    }
-}
+  init(el) {
+    app.start(el);
+  }
+};
