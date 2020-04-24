1b4ea86cc5f9bd48cdcead2e4760005e08aca730
qiaoyue <qiaoyue@baidu.com>
2020-04-24 13:39:07 +0800

    update

--- src/redux-imitate/store.ts
+++ src/redux-imitate/store.ts
@@ -3,54 +3,56 @@
  */
 
 function createStore(state, stateChanger) {
-    const listeners = [];
-    const subscribe = (listener) => listeners.push(listener);
-    const getState = () => state;
-    const dispatch = (action) => {
-        state = stateChanger(state, action);
-        listeners.forEach(listener => listener(state));
-    };
-    return { getState, dispatch, subscribe };
+  const listeners = [];
+  const subscribe = (listener) => listeners.push(listener);
+  const getState = () => state;
+  const dispatch = (action) => {
+    // 执行 reducer 生成新的 state
+    state = stateChanger(state, action);
+    // 触发 subscribe 监听
+    listeners.forEach((listener) => listener(state));
+  };
+  return { getState, dispatch, subscribe };
 }
 
 function stateChanger(state, action) {
-    switch (action.type) {
-        case 'UPDATE_HEADER_TEXT':
-            return {
-                ...state,
-                header: {
-                    ...state.header,
-                    text: action.text
-                }
-            };
-        case 'UPDATE_HEADER_COLOR':
-            return {
-                ...state,
-                header: {
-                    ...state.header,
-                    color: action.color
-                }
-            };
-        case 'UPDATE_CONTENT_TEXT':
-            return {
-                ...state,
-                content: {
-                    ...state.content,
-                    text: action.text
-                }
-            };
-    }
+  switch (action.type) {
+    case 'UPDATE_HEADER_TEXT':
+      return {
+        ...state,
+        header: {
+          ...state.header,
+          text: action.text,
+        },
+      };
+    case 'UPDATE_HEADER_COLOR':
+      return {
+        ...state,
+        header: {
+          ...state.header,
+          color: action.color,
+        },
+      };
+    case 'UPDATE_CONTENT_TEXT':
+      return {
+        ...state,
+        content: {
+          ...state.content,
+          text: action.text,
+        },
+      };
+  }
 }
 
 const appState = {
-    header: {
-        text: 'this is title',
-        color: 'red',
-    },
-    content: {
-        text: 'this is content',
-        color: 'red'
-    }
+  header: {
+    text: 'this is title',
+    color: 'red',
+  },
+  content: {
+    text: 'this is content',
+    color: 'red',
+  },
 };
 
 export default createStore(appState, stateChanger);
