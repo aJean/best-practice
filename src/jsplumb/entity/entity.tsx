57cc33214927430d2cad2fe6430dba5216c2c124
qiaoyue <qiaoyue@guanghe.tv>
2020-05-06 12:38:43 +0800

    feat: new

--- src/jsplumb/entity/entity.tsx
+++ src/jsplumb/entity/entity.tsx
@@ -6,15 +6,35 @@ import Option from '../common/option';
  */
 
 export default class Entity extends React.Component<any, any> {
-    generateOptions() {
-        const options = this.props.options;
+  generateOptions() {
+    const options = this.props.options;
 
-        return options ? this.props.options.map(data => 
-            <Option id={data.id} key={data.id} text={data.text} />)
-            : null;
-    }
+    return options ? this.props.options.map((data) => <Option id={data.id} key={data.id} text={data.text} />) : null;
+  }
 
-    render() {
-        return null;
-    }
+  render() {
+    return null;
+  }
 }
+
+
+// 泛型约束返回值
+interface ResponseData<T> {
+  code: number;
+  result: T;
+  message: string;
+}
+
+interface IUser {
+  name: string;
+}
+
+function getUser<T>(): ResponseData<T> {
+  return;
+}
+
+function testUser(): ResponseData<IUser> {
+  return getUser();
+}
+
+testUser().result.name;
