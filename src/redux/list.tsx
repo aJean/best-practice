a31e4c85cb68ec01225660b4a8426c507ea27ca2
aJean <30001677@qq.com>
2020-06-23 14:54:48 +0800

    fix: change

--- src/redux/list.tsx
+++ src/redux/list.tsx
@@ -8,44 +8,44 @@ import Container from '../hooks/container';
  * @file withApollo 有没有必要，是否应该自己获取 client
  */
 
-const mapStateToProps = state => {
-    return {list: state.list, user: state.user};
+const mapStateToProps = (state) => {
+  return { list: state.list, user: state.user };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onAddList: () => dispatch(actions.fetchData({url: true})),
-        onAddUser: () => dispatch(actions.fetchUser({user: true})),
-        onAddDb: () => dispatch(actions.fetchDb({server: false}))
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAddList: () => dispatch(actions.fetchData({ url: true })),
+    onAddUser: () => dispatch(actions.fetchUser({ user: true })),
+    onAddDb: () => dispatch(actions.fetchDb({ server: false }))
+  };
 };
 
 // hooks + redux
-const Btnf = props => {
-    const dispatch = useDispatch();
-    const clickHandle = function() {
-        dispatch(actions.fetchData({url: true}));
-    }
- 
-    return <button onClick={clickHandle}>测试 hook item</button>
+const Btnf = (props) => {
+  const dispatch = useDispatch();
+  const clickHandle = function () {
+    dispatch(actions.fetchData({ url: true }));
+  };
+
+  return <button onClick={clickHandle}>测试 hook item</button>;
 };
 
 class List extends React.PureComponent<any, any> {
-    componentDidMount() {
-        // getPost(this.props.client, 1).then(res => console.log(res));
-    }
+  componentDidMount() {
+    // getPost(this.props.client, 1).then(res => console.log(res));
+  }
 
-    render() {
-        return (
-            <div>
-                <Container list={this.props.list} />
-                <h1>{this.props.user}翻你的排</h1>
-                <Btnf />
-                <button onClick={this.props.onAddUser}>添加 user</button>
-                <button onClick={this.props.onAddDb}>测试 db</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Container list={this.props.list} />
+        <h1>{this.props.user}翻你的排</h1>
+        <Btnf />
+        <button onClick={this.props.onAddUser}>添加 user</button>
+        <button onClick={this.props.onAddDb}>测试 db</button>
+      </div>
+    );
+  }
 }
 
 export default compose(withApollo, connect(mapStateToProps, mapDispatchToProps))(List);
